073d87013c872111f2a3483597df1478d20b1ead
ViniciusPii <vinysales@hotmail.com>
2020-08-29 11:43:47 -0300

    [Melhorias] Melhorias nos padões do  MVVM

--- app/src/main/java/com/viniciuspiih/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/viniciuspiih/calculadoradeimc/MainActivity.kt
@@ -3,15 +3,18 @@ package com.viniciuspiih.calculadoradeimc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+
+    private val messageTextView: TextView by lazy { text_result_message }
+    private val resultTextView: TextView by lazy { text_result }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,31 +25,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         viewModel.imc.observe(this, Observer {
-            text_result.text = "%.2f".format(it)
-            text_result.visibility = View.VISIBLE
+            resultTextView.text = getString(R.string.format).format(it)
+            resultTextView.visibility = View.VISIBLE
         })
 
         viewModel.imcResult.observe(this, Observer {
-            text_result_message.text = it
-            text_result_message.visibility = View.VISIBLE
+            messageTextView.text = it
+            messageTextView.visibility = View.VISIBLE
         })
 
-        button_calculate.setOnClickListener(this)
-    }
+        button_calculate.setOnClickListener {
+            if (validationOk()) {
+                val weight = edit_weight.text.toString().toFloat()
+                val height = edit_height.text.toString().toFloat()
 
-    override fun onClick(v: View?) {
-        val id = v?.id
-        if (id == R.id.button_calculate) {
-            calculateImc()
+                viewModel.calculateIMC(weight, height, this)
+            } else {
+                Toast.makeText(applicationContext, getString(R.string.empty_fields), Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
-    private fun calculateImc() {
-        val weight = edit_weight.text.toString().toFloat()
-        val height = edit_height.text.toString().toFloat()
-
-        viewModel.calculateIMC(weight, height)
+    private fun validationOk(): Boolean {
+        return (edit_weight.text.toString() != "" && edit_height.text.toString() != "")
     }
 
-
 }
+
--- app/src/main/java/com/viniciuspiih/calculadoradeimc/MainViewModel.kt
+++ app/src/main/java/com/viniciuspiih/calculadoradeimc/MainViewModel.kt
@@ -1,31 +1,33 @@
 package com.viniciuspiih.calculadoradeimc
 
+import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    private var mImc = MutableLiveData<Float>()
-    var imc = mImc
+    private var _imc = MutableLiveData<Float>()
+    var imc: LiveData<Float> = _imc
 
-    private var mImcResult = MutableLiveData<String>()
-    var imcResult = mImcResult
+    private var _imcResult = MutableLiveData<String>()
+    var imcResult: LiveData<String> = _imcResult
 
-    fun calculateIMC(weight: Float, height: Float) {
+    fun calculateIMC(weight: Float, height: Float, context: Context) {
         val result = (weight / (height * height))
 
         when {
-            result <= 17 -> imcResult.value = "Muito abaixo do Peso!"
-            result <= 18.49 -> imcResult.value = "Abaixo do Peso!"
-            result <= 24.99 -> imcResult.value = "Peso Normal!"
-            result <= 29.99 -> imcResult.value = "Acima do Peso!"
-            result <= 34.99 -> imcResult.value = "Obesidade I"
-            result <= 39.99 -> imcResult.value = "Obesidade II"
-            else -> imcResult.value = "Obesidade III"
+            result <= 17 -> _imcResult.value = context.getString(R.string.very_low)
+            result <= 18.49 -> _imcResult.value = context.getString(R.string.below)
+            result <= 24.99 -> _imcResult.value = context.getString(R.string.normal)
+            result <= 29.99 -> _imcResult.value = context.getString(R.string.above)
+            result <= 34.99 -> _imcResult.value = context.getString(R.string.obesity_one)
+            result <= 39.99 -> _imcResult.value = context.getString(R.string.obesity_two)
+            else -> _imcResult.value = context.getString(R.string.obesity_three)
         }
 
 
-        mImc.value = result
+        _imc.value = result
     }
 
 }
